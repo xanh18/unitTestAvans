019b9f4dfb95301e91e15a72344d80099686ac81
xanh18 <33584278+xanh18@users.noreply.github.com>
2023-06-09 23:06:04 +0200

    test1

---
+++ unitTestAvans/ShippingCostsCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingCostsCalculator
+{
+    public class ClassAssignmentAvans
+    {
+        public double ShippingCosts(bool calculateShippingCosts, string typeOfShippingCosts, double totalPrice)
+        {
+            Console.WriteLine("doobedoo");
+            double result = 0;
+            if (calculateShippingCosts == true)
+            {
+                if (totalPrice > 1500)
+                {
+                    result = 0;
+                }
+                else
+                {
+                    switch (typeOfShippingCosts)
+                    {
+                        case "Ground":
+                            result = 100;
+                            break;
+                        case "InStore":
+                            result = 50;
+                            break;
+                        case "NextDayAir":
+                            result = 250;
+                            break;
+                        case "SecondDayAir":
+                            result = 125;
+                            break;
+                        default:
+                            result = 0;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+
+}
